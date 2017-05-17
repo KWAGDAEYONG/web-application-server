b78209fcc850b7ccd18cd7fb2e8b609f2d644159
kwagdaeyong <dayong7387@hanmail.net>
2017-05-17 20:22:01 +0900

    post

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,13 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,20 +28,34 @@ public class RequestHandler extends Thread {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
             String line = bufferedReader.readLine();
-            String requetUrl = "";
-            while (!"".equals(line)) {
-                if(line==null){
-                    return;
-                }
-                log.debug("header : {}", line);
-                String token[] = line.split(" ");
-                if(line.startsWith("GET")){
-                    requetUrl = token[1];
-                }
-                line = bufferedReader.readLine();
+            String token[] = line.split(" ");
+            String url = token[1];
+
+            if(line==null){
+                return;
             }
+            int conLen = 0;
+            while(!"".equals(line)){
+                log.debug("header:{}",line);
+                line = bufferedReader.readLine();
+                //post일때 읽기
+                if(line.startsWith("Content-Length")){
+                    String temp[] = line.split(" ");
+                    conLen = Integer.parseInt(temp[1]);
+                }
+            }
+
+            if(url.contains("/create")){
+                String data = IOUtils.readData(bufferedReader,conLen);
+                Map<String, String> param = HttpRequestUtils.parseQueryString(data);
+                User user = new User(param.get("userId"),param.get("password"),param.get("name"),param.get("email"));
+                log.debug("user:{}",user.toString());
+                url = "/index.html";
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp"+requetUrl).toPath());
+            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
